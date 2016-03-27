bc42f3843625d4b8ad3c37ae9c83b27b26c98328
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-03-27 17:16:15 -0300

    Implementacao de lista duplamente encadeada com nos e iteradores.

---
+++ ListasLineares/src/examples/ExemploListaEncadeada.java
@@ -0,0 +1,32 @@
+package examples;
+
+import datastructures.IteradorListaEncadeada;
+import datastructures.ListaEncadeada;
+
+public class ExemploListaEncadeada {
+
+	public static void main(String[] args) {
+		ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+		
+		lista.append(3);
+		lista.prepend(1);	
+		imprimeLista(lista);
+		IteradorListaEncadeada<Integer> iterador = lista.iterator();
+		iterador.next();
+		iterador.insert(2);
+		imprimeLista(lista);
+		iterador.next();
+		iterador.insert(4);
+		imprimeLista(lista);
+		iterador.previous();
+		iterador.remove();
+		imprimeLista(lista);
+	}
+
+	private static <T> void imprimeLista(ListaEncadeada<T> lista) {
+		for (T dado : lista)
+			System.out.println(dado);
+		System.out.println("-----------------");
+	}
+
+}
