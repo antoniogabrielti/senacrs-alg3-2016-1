214bd3ca004b84490d943fe81e0e7517c615b934
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-05-04 16:20:55 -0300

    Classe Paciente, como necessaria para leitura do arquivo.

---
+++ ConsultorioMedico/src/model/Paciente.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+	private String nome;
+	private String rg;
+	private LocalDate dataNascimento;
+
+	public void setNome(String nome) {
+		if (nome != null)
+			this.nome = nome;
+	}
+
+	public void setRG(String rg) {
+		if (rg != null)
+			this.rg = rg;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		if (dataNascimento.isBefore(LocalDate.now()) &&
+			dataNascimento.getYear() > 1850)
+				this.dataNascimento = dataNascimento;
+	}
+
+}
