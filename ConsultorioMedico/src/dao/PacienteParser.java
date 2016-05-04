0c68e930d526e8ef7f3428f43d1e6b467693538e
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-05-04 16:21:59 -0300

    Classe para criacao do objeto Paciente a partir do arquivo CSV.

---
+++ ConsultorioMedico/src/dao/PacienteParser.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import model.Paciente;
+import util.CSVParser;
+
+public class PacienteParser implements CSVParser<Paciente> {
+
+	private static final String DATE_FORMAT = "yyy-MM-dd";
+
+	@Override
+	public Paciente parseObject(String csvdata) {
+		Scanner scanner = new Scanner(csvdata);
+		scanner.useDelimiter(";");
+		Paciente paciente = new Paciente();
+		paciente.setNome(scanner.next());
+		paciente.setRG(scanner.next());
+		paciente.setDataNascimento(convertDate(scanner.next()));
+		return paciente;
+	}
+
+	private LocalDate convertDate(String date) {
+		DateTimeFormatter format = DateTimeFormatter.ofPattern(DATE_FORMAT);
+		return LocalDate.parse(date, format);
+	}
+
+}
